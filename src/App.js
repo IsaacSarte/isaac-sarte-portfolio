d36ea812678e621e01f986e24f09403b6b93bb32
Isaac Sarte <aizakku09@gmail.com>
2022-02-18 12:58:56 +0800

    change of plans

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React from "react";
-import {Canvas} from "react-three-fiber";
-import {OrbitControls, Stars} from "drei";
+import React from 'react'
 
-// CSS
-import './App.css';
-
-// Components
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Arrow from "./components/arrow/Arrow";
-import Home from "./components/home/Home";
-
-export default function App() {
-    return (
-        <>
-            <Header />
-                <Canvas>
-                    <OrbitControls />
-                    <Stars 
-                        depth={50}
-                        count={2500}
-                        fade
-                    />
-                </Canvas>
-                <Home />
-                <Arrow />
-            <Footer />
-        </>
-    );
+const App = () => {
+  return (
+    <div>
+        App
+    </div>
+  )
 }
+
+export default App;
